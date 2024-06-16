b7a82c0eece6febfe906495dcc5a0df9877afc2a
Jack Camas <jackcamasdev@gmail.com>
2024-06-16 18:50:43 -0400

    Initial Commit

---
+++ personal-test/personal_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  personal_testApp.swift
+//  personal-test
+//
+//  Created by Jack Camas on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct personal_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
